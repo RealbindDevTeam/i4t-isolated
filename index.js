2049e594cd8e6a4b9fe08dd15566b8139aee9af3
leonardoespinosa <leonardoespinosa@MacBook-Pro-de-Leonardo.local>
2017-09-01 12:05:55 -0500

    Add filter to authorize development and testing environments IPs

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-// Declaración de variables
+// Variables Declaration
 var express = require('express');
-var app = express();
+var ipfilter = require('express-ipfilter').IpFilter;
+var IpDeniedError = require('express-ipfilter').IpDeniedError;
+var path = require('path');
 var payu = require('./server/payu.js');
 const bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'view engine', 'jade' );
 
 // Middlewares
 app.use( bodyParser.json() ); //for request application/json  
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
-// Obtiene la vista root del proyecto y ejecuta request o response
+// Whitelist the following IPs -> Development - Testing Environments
+var ips = [ '35.185.93.165', '35.185.118.219' ];
+app.use( ipfilter( ips, { mode: 'allow' } ) );
+
+// Error handler
+app.use( function(err, req, res, _next ) {
+  if( err instanceof IpDeniedError ){
+    res.status(401);
+  }else{
+    res.status( err.status || 500 );
+  }
+  err.name = 'Error. IP Address Not authorized';
+  res.send( err );
+});
+
+// Get view root project and exec request o response
 app.get( '/', function( req, res ){
-    res.send( 'Iurest Isolated' );
+  res.send( 'Iurest Isolated' );
 });
 
-// Retorna la informacion de las llaves de PayU
+// Get PayU keys Information
 app.get( '/api/getCusPayInfo', function( req, res ){
-    res.status(200).jsonp( payu.getKeys() );
+  res.status(200).jsonp( payu.getKeys() );
 });
 
-//Habilitacion de puerto 9000
+// Allows 9000 port
 app.listen( 9000 );
