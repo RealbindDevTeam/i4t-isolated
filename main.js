b4376183f4dfecc797eb09d151e979b8312b3f3f
Leonardo González <clgrhc@gmail.com>
2017-10-18 02:56:28 -0500

    added cors validation for valid origins

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ var IpDeniedError = require('express-ipfilter').IpDeniedError;
 var path = require('path');
 var payu = require('./server/payu.js');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
+
+var corsOptions = {
+  origin:  [ 'http://35.185.93.165:8080', 'http://35.185.118.219:8080' ],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
 
 // view engine setup
 app.set( 'views', path.join( __dirname, 'views' ) );
@@ -37,7 +43,7 @@ app.get( '/', function( req, res ){
 });
 
 // Get PayU keys Information
-app.get( '/api/getCusPayInfo', function( req, res ){
+app.get( '/api/getCusPayInfo', cors(corsOptions),function( req, res ){
   res.status(200).jsonp( payu.getKeys() );
 });
 
